ff5f4c2824fcb9b10bb94f11d4eb2246945633ad
Dev12820 <devansh7865@gmail.com>
2022-09-27 22:07:23 +0530

    First commit

---
+++ diff.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+int main()
+    {
+            int m,n;
+    int arr[10],brr[10],crr[20];
+    int i=0,j=0,k=0,tmp,pm;
+    scanf("%d",&m);
+    scanf("%d",&n);
+            for(int i=0;i<m;i++)
+            {
+                scanf("%d",&arr[i]);
+            }
+            for(int i=0; i<m; i++)
+    {
+        for(int j=i+1; j<m; j++)
+        {
+            if(arr[j] <arr[i])
+            {
+                tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
+            }
+        }
+    }
+        for( int i=0; i<n; i++)
+    {
+        for(int j=i+1; j<n; j++)
+        {
+            if(brr[j] <brr[i])
+            {
+                pm = brr[i];
+                brr[i] = brr[j];
+                brr[j] = pm;
+            }
+        }
+    }
+                for(int j=0;j<n;j++)
+                {
+                    scanf("%d",&brr[j]);
+                }
+                     while(i<m&&j<n)
+                    {
+                        if(arr[i]<brr[j])
+                        
+                            crr[k++]=arr[i++];
+                           else if(brr[j]<arr[i])
+                           j++;
+                           else
+                           {
+                           i++;
+                           j++;
+                           }
+                        
+                            
+                    }
+                        while(i<m)
+                        {
+                            crr[k]=arr[i];
+                            k=k+1;
+                            i=i+1;
+                        }
+                            
+                    }
+                    
+                            for(int z=0;z<k;z++)
+                            printf("%d",crr[z]);
+                        }
+    
+
